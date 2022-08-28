796016c884b53d6c73d159a2ca64b70cd3e45184
ajardev <gudjier@gmail.com>
2022-08-28 21:11:14 +0700

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,17 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-postcss',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Ajar Parama',
+        short_name: 'Ajar Parama',
+        start_url: '/',
+        background_color: 'white',
+        theme_color: 'black',
+        display: 'minimal-ui',
+        icon: 'src/images/logo-square.png',
+      },
+    },
   ],
 }
